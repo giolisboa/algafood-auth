9fb83605936c0abe47464162fb4885806b05a683
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-12-15 22:28:10 -0300

    23.14. Autenticando usuário com dados do banco de dados

---
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.auth.core;
+
+import java.util.Collections;
+
+import org.springframework.security.core.userdetails.User;
+
+import com.algaworks.algafood.auth.domain.Usuario;
+
+public class AuthUser extends User {
+
+    private static final long serialVersionUID = 1L;
+
+    private String fullName;
+
+    public AuthUser(Usuario usuario) {
+        super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+
+        this.fullName = usuario.getNome();
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+}
---
+++ src/main/java/com/algaworks/algafood/auth/domain/Usuario.java
@@ -0,0 +1,76 @@
+package com.algaworks.algafood.auth.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        return Objects.equals(id, other.id);
+    }
+
+}
